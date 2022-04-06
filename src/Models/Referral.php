87869b5fc48d51591abfe5f2c999a41691193a43
Maximilian Kern <m.kern@mke-media.de>
2022-04-06 17:04:23 +0200

    V1

--- src/Models/Referral.php
+++ src/Models/Referral.php
@@ -1,6 +1,11 @@
 <?php
 namespace Go2Flow\SaasRegisterLogin\Models;
 
+use Spatie\Translatable\HasTranslations;
+
 class Referral extends \Go2Flow\SaasRegisterLogin\Models\AbstractModels\AbstractReferral
 {
+    use HasTranslations;
+
+    public $translatable = ['name'];
 }
