9fd8307fa2554a6a17725643d5a59486b9142337
sassm <m.sass@mke-media.de>
2022-04-07 12:26:19 +0200

    V1 WIP

--- src/Models/Team/Invitation.php
+++ src/Models/Team/Invitation.php
@@ -1,6 +1,12 @@
 <?php
 namespace Go2Flow\SaasRegisterLogin\Models\Team;
 
+use Spatie\Permission\Models\Role;
+
 class Invitation extends \Go2Flow\SaasRegisterLogin\Models\Team\AbstractModels\AbstractInvitation
 {
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
 }
