e6149a9fd074e59f866cf1bcb36c697795567406
Maximilian Kern <m.kern@mke-media.de>
2022-05-02 13:28:57 +0200

    V1

--- routes/api/team.php
+++ routes/api/team.php
@@ -1,12 +1,5 @@
 <?php
 
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\PermissionController;
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\ReferralController;
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\TeamController;
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\UserController;
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\ValidationController;
-use Go2Flow\SaasRegisterLogin\Http\Controllers\API\WorldCountryCurrencyController;
 use Illuminate\Support\Facades\Route;
 
 //Route::post('/team/{team}/tokens/create/', [TeamController::class, 'createToken']);
-//Route::get('/team/{team}/tokens/delete/{tokenId}', [TeamController::class, 'deleteToken']);
--- src/Http/Middleware/TeamsPermission.php
+++ src/Http/Middleware/TeamsPermission.php
@@ -2,6 +2,7 @@
 
 namespace Go2Flow\SaasRegisterLogin\Http\Middleware;
 
+use Go2Flow\SaasRegisterLogin\Models\Team;
 use Go2Flow\SaasRegisterLogin\Models\User;
 
 class TeamsPermission {
@@ -12,6 +13,14 @@ class TeamsPermission {
         if(!empty(auth()->user()) && auth()->user() instanceof User){
             if (getSaasTeamId() === null) {
                 setSaasTeamId(auth()->user()->teams->first()->id);
+            }
+            // session value set on login
+            setPermissionsTeamId(session('team_id'));
+        }
+
+        if(!empty(auth()->user()) && auth()->user() instanceof Team){
+            if (getSaasTeamId() === null) {
+                setSaasTeamId(auth()->user()->id);
             }
             // session value set on login
             setPermissionsTeamId(session('team_id'));
