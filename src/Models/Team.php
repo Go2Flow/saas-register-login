cdc222caf8073053d63eb06154b3a94572fe595e
Maximilian Kern <m.kern@mke-media.de>
2022-05-11 14:14:44 +0200

    V1

--- src/Models/Team.php
+++ src/Models/Team.php
@@ -4,14 +4,36 @@ namespace Go2Flow\SaasRegisterLogin\Models;
 use Go2Flow\SaasRegisterLogin\Database\Factories\TeamFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Team extends \Go2Flow\SaasRegisterLogin\Models\AbstractModels\AbstractTeam
+class Team extends \Go2Flow\SaasRegisterLogin\Models\AbstractModels\AbstractTeam  implements HasMedia
 {
-    use HasFactory, HasApiTokens;
+    use HasFactory, HasApiTokens, InteractsWithMedia;
+
+    const MEDIA_LOGO = 'logo';
+    const MEDIA_CONVERSION_LOGO = 'logo';
 
     /** @return TeamFactory */
     protected static function newFactory()
     {
         return TeamFactory::new();
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection(self::MEDIA_LOGO)
+            ->singleFile();
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion(self::MEDIA_CONVERSION_LOGO)
+            ->fit(Manipulations::FIT_FILL, 1000, 320)
+            ->nonQueued()->performOnCollections(self::MEDIA_LOGO);
     }
 }
