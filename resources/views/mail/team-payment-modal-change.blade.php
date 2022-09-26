1acac2901558a8861d357d20b5108b350babc26f
sassm <m.sass@mke-media.de>
2022-09-26 18:13:57 +0200

    - add payment model change mail

--- resources/views/mail/team-payment-modal-change.blade.php
+++ resources/views/mail/team-payment-modal-change.blade.php
@@ -3,8 +3,13 @@
     /* @var \Go2Flow\SaasRegisterLogin\Models\Team $team */
 @endphp
 Teamname: {{$team->name}}
+
 Team id: {{$team->id}}
+
 PSP Id: {{$team->psp_id}}
+
 PSP Instanz: {{$team->psp_instance}}
+
 Neues Modell: {{$team->payment_model}}
+
 @endcomponent
