d86c3978e0445c46e6fa18a4e7423207478b225a
Maximilian Kern <m.kern@mke-media.de>
2023-03-08 14:13:02 +0100

    Refactoring

---
+++ database/migrations/2022_11_17_103543_change_nullable_fields.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('psp_id', 64)->nullable(true)->change();
+            $table->string('psp_instance')->nullable(true)->change();
+            $table->string('billing_address', 255)->nullable(true)->change();
+            $table->string('billing_city', 255)->nullable(true)->change();
+            $table->string('billing_postal_code', 25)->nullable(true)->change();
+            $table->string('billing_country', 2)->nullable(true)->change();
+            $table->string('phone_prefix', 5)->nullable(true)->change();
+            $table->string('phone_number', 50)->nullable(true)->change();
+            $table->string('currency', 3)->nullable(true)->change();
+            $table->json('languages')->default(['de'])->change();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+          //
+        });
+    }
+};
